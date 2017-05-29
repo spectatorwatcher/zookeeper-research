36758db839596d67c52c24933761fd92ee7517dd
spectatorwatcher <spectator.watcher@gmail.com>
2017-05-29 18:12:48 +0800

    迁移zk工程，并删除所有java无关部分，在本地验证客户端部分可用后初始化

---
+++ src/java/main/org/apache/zookeeper/version/Info.java
@@ -0,0 +1,12 @@
+// Do not edit!
+package org.apache.zookeeper.version;
+
+public interface Info {
+    public static final int MAJOR=3;
+    public static final int MINOR=4;
+    public static final int MICRO=6;
+    public static final String QUALIFIER=null;
+    public static final String REVISION_HASH=null;
+    public static final int REVISION=1569965;
+    public static final String BUILD_DATE="02/23/2014 17:18 GMT";
+}
